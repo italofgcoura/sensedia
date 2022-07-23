13d4b93864da169e2a574e8dd7cbde7d9368ae9f
italofgcoura <33141562+italofgcoura@users.noreply.github.com>
2022-07-23 14:30:21 -0300

    Test automatic deploy

--- src/components/Search/styles.ts
+++ src/components/Search/styles.ts
@@ -5,7 +5,8 @@ export const InputSearch = styled.input`
   border-bottom: 1px solid ${({ theme }) => theme.colors.gray};
   background-color: ${({ theme }) => theme.colors.lightGray};
   padding: 7.5px 16px;
-  max-width: 621px;
+  max-width: 620px;
   width: 100%;
   font-size: 16px;
+
 `;
